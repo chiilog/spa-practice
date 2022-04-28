e277d53b5cef4baff5472d9d91fd78d63a061db1
Chiaki Obara <obara@ham.works>
2022-04-28 17:31:26 +0900

    手順書コメントにのこす

--- components/PrefecturesList.tsx
+++ components/PrefecturesList.tsx
@@ -1,23 +1,41 @@
-import React, { useState } from "react";
+import React from "react";
 
 interface PrefecturesListProps {
   prefectures: {
     prefCode: number;
     prefName: string;
   }[];
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
+/**
+ * input + 都道府県名のリストを表示して、チェック状態を取得する
+ * - prefectures で渡ってきた引数から都道府県一覧を表示する
+ * - チェックされた都道府県の prefCode を渡せるようにする
+ *
+ * @param prefectures
+ * @param onChange
+ * @constructor
+ */
 export const PrefecturesList: React.FC<PrefecturesListProps> = ({
   prefectures,
-}) => (
-  <div className="grid grid-cols-4 gap-4">
-    {prefectures?.map(({ prefName }) => (
-      <div key={prefName}>
-        <label>
-          <input type="checkbox" name="prefectures" value={prefName} />
-          <span className="pl-2">{prefName}</span>
-        </label>
-      </div>
-    ))}
-  </div>
-);
+  onChange,
+}) => {
+  return (
+    <div className="grid grid-cols-4 gap-4">
+      {prefectures?.map(({ prefCode, prefName }) => (
+        <div key={prefName}>
+          <label>
+            <input
+              type="checkbox"
+              name="prefectures"
+              value={prefCode}
+              onChange={onChange}
+            />
+            <span className="pl-2">{prefName}</span>
+          </label>
+        </div>
+      ))}
+    </div>
+  );
+};
