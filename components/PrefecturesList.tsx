e27a69a50e5226f9d00d1b4db37281fae1fc631c
Chiaki Obara <obara@ham.works>
2022-04-28 12:13:14 +0900

    PrefecturesListの型修正

--- components/PrefecturesList.tsx
+++ components/PrefecturesList.tsx
@@ -1,19 +1,17 @@
 import React, { useState } from "react";
 
 interface PrefecturesListProps {
-  prefectures:
-    | {
-        prefCode: number;
-        prefName: string;
-      }[]
-    | [];
+  prefectures: {
+    prefCode: number;
+    prefName: string;
+  }[];
 }
 
 export const PrefecturesList: React.FC<PrefecturesListProps> = ({
   prefectures,
 }) => (
   <div className="grid grid-cols-4 gap-4">
-    {prefectures.map(({ prefName }) => (
+    {prefectures?.map(({ prefName }) => (
       <div key={prefName}>
         <label>
           <input type="checkbox" name="prefectures" value={prefName} />
