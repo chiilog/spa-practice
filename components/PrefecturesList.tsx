c7687935168f8d9eadc5f41dd63f0791107e2fa4
Chiaki Obara <obara@ham.works>
2022-04-28 12:04:00 +0900

    PrefecturesListリファクタリング

--- components/PrefecturesList.tsx
+++ components/PrefecturesList.tsx
@@ -6,23 +6,20 @@ interface PrefecturesListProps {
         prefCode: number;
         prefName: string;
       }[]
-    | null;
+    | [];
 }
 
 export const PrefecturesList: React.FC<PrefecturesListProps> = ({
   prefectures,
-}) => {
-  return (
-    <div className="grid grid-cols-4 gap-4">
-      {prefectures &&
-        prefectures.map(({ prefName }) => (
-          <div key={prefName}>
-            <label>
-              <input type="checkbox" name="prefectures" value={prefName} />
-              <span className="pl-2">{prefName}</span>
-            </label>
-          </div>
-        ))}
-    </div>
-  );
-};
+}) => (
+  <div className="grid grid-cols-4 gap-4">
+    {prefectures.map(({ prefName }) => (
+      <div key={prefName}>
+        <label>
+          <input type="checkbox" name="prefectures" value={prefName} />
+          <span className="pl-2">{prefName}</span>
+        </label>
+      </div>
+    ))}
+  </div>
+);
