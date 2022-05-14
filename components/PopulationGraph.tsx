02d1ed0dda1a4b03bc78b2befad79cbdd7ea7566
Chiaki Obara <obara@ham.works>
2022-05-14 17:22:46 +0900

    PopulationGraphコンポーネントは渡された値を表示するだけにする（渡される値未実装のため一旦空）

--- components/PopulationGraph.tsx
+++ components/PopulationGraph.tsx
@@ -1,23 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { FC } from "react";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
-import axios from "axios";
-
-import { prefDataProps } from "../type/pref";
-
-interface PopulationGraphProps {
-  checkedPrefectures: prefDataProps[];
-}
-
-interface populationProps {
-  name?: string;
-  data?: number[];
-}
 
 /**
- * データ提供年
- * - RESAS側で1980-2045年（5年毎）の提供。
- * - データ取得されるまで入らないのでベタで作成する
+ * データ提供年（横軸）の変数を作成する
+ *  - RESAS側で1980-2045年（5年毎）の提供。
+ *  - データ取得されるまで入らないのでベタで作成する
  */
 const categories: number[] = [];
 for (let i = 1980; i <= 2045; i = i + 5) {
@@ -25,76 +13,12 @@ for (let i = 1980; i <= 2045; i = i + 5) {
 }
 
 /**
- * チェックされた都道府県の総人口数のグラフを作成する
- * -[x] 渡されたcheckedPrefecturesの値をもとにRESASの人口構成APIを使用して都道府県別の配列を作成する
- *   -[x] Highchartsのoptionで都道府県名が必要なので、都道府県名も忘れずオブジェクトに入れておく
- * - 取得した都道府県別の人口構成から必要なデータのみに絞った配列を作成する
- *   -[x] {name: 都道府県名, data: "RESASの総人口の配列の中身"} の配列を作成する
- *      - HighchartsReact のoption -> series （都道府県別人口）に入れる
- *   - [x]年度（横軸）の配列を作成する
- *      - [x]HighchartsReact のoption -> categories （年度）に入れる
- * -[] チェックを消したときにグラフを消す
- *   -[] population の配列の中にチェックを消した都道府県が存在するかチェックする
- *   -[] 存在しない場合、filterで該当する都道府県を含まない配列を作成する
+ * チェックされた都道府県の総人口数のグラフを表示する
  *
  * @param checkedPrefectures
  * @constructor
  */
-export const PopulationGraph: React.FC<PopulationGraphProps> = ({
-  checkedPrefectures,
-}) => {
-  const [population, setPopulation] = useState<populationProps[]>([]);
-
-  useEffect(() => {
-    checkedPrefectures?.map(({ prefCode, prefName, checked }) => {
-      // TODO: url -> `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=${prefCode}`;
-      const url = "../dammy/perYear.json";
-
-      axios
-        .get(url, {
-          headers: { "X-API-KEY": `${process.env.NEXT_PUBLIC_RESAS_API_KEY}` },
-        })
-        .then((res) => {
-          if (res.data.statusCode === "403" || res.data.statusCode === "404") {
-            return;
-          }
-
-          const result: { year: number; value: number }[] =
-            res.data.result.data[0].data;
-          const data: number[] = [];
-
-          result.map(({ value }) => {
-            data.push(value);
-          });
-
-          // TODO: コードが冗長では？
-          // FIXME: チェックをはずすときにpopulationがうまく更新できてない
-          if (checked) {
-            if (!population.find(({ name }) => name === prefName)) {
-              setPopulation([
-                ...population,
-                {
-                  name: prefName,
-                  data: data,
-                },
-              ]);
-            }
-          } else {
-            // FIXME: この時点でクリックバグある
-            console.log(`${prefName}をけしたい`);
-            setPopulation(
-              population.filter(({ name }) => {
-                return name !== prefName;
-              })
-            );
-          }
-        });
-    });
-  }, [checkedPrefectures]);
-
-  console.log(population);
-  console.log(checkedPrefectures);
-
+export const PopulationGraph: FC = () => {
   const options = {
     title: null,
     xAxis: {
@@ -115,7 +39,7 @@ export const PopulationGraph: React.FC<PopulationGraphProps> = ({
         },
       ],
     },
-    series: population,
+    series: [{}],
   };
 
   return (
